a0692064ee02ea58ab02163371bf57bc4b91ba33
Chintha2525 <dinesharun2525@gmail.com>
2023-11-09 14:11:49 +0530

    initial commit

---
+++ src/context/Datacontext.js
@@ -0,0 +1,94 @@
+import { createContext, useState } from "react";
+
+
+
+const Datacontext=createContext({});
+
+export const DataProvider=({children})=>{
+
+    
+  const [edittea, setEdittea] = useState([])
+
+  const [editstu, setEditstu] = useState([])
+
+  const [student, addStudent] = useState([
+    {
+      id: 1,
+      name: "Chintha",
+      fathername: "Gnanasekar",
+      class: "I",
+      address: "Erode",
+      teacher: "Arun",
+    },
+    {
+      id: 2,
+      name: "Jega",
+      fathername: "Murugesh",
+      class: "I",
+      address: "Erode",
+      teacher: "Arun",
+    },
+    {
+      id: 3,
+      name: "Prabhu",
+      fathername: "somu",
+      class: "I",
+      address: "Erode",
+      teacher: "Arun",
+    },
+  ]);
+
+  const [teacher, addTeacher] = useState([
+    {
+      id: 1,
+      name: "Arun",
+      master: "Tamil",
+      address: "Erode",
+    },
+  ]);
+
+
+
+  const [style, setStyle] = useState(
+    "navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+  );
+
+  const Changestyle = () => {
+    if (
+      style === "navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+    ) {
+      setStyle(
+        "navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled"
+      );
+    } else {
+      setStyle("navbar-nav bg-gradient-primary sidebar sidebar-dark accordion");
+    }
+  };
+  const Changestyle1 = () => {
+    if (
+      style === "navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+    ) {
+      setStyle(
+        "navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled1"
+      );
+    } else {
+      setStyle("navbar-nav bg-gradient-primary sidebar sidebar-dark accordion");
+    }
+  };
+
+
+    return(
+        <Datacontext.Provider value={{
+                style,Changestyle,Changestyle1,
+                student,addStudent,
+                editstu,setEditstu,
+                teacher,addTeacher,
+                edittea,setEdittea
+        }}>
+            {children}
+           
+        </Datacontext.Provider>
+    )
+}
+
+export default Datacontext
